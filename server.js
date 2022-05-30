c1df8e8afb80cc2be273d9a39bc33be65b8125bc
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2022-05-30 22:36:49 +0700

    feat: edit server.js

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = process.env.HOSTNAME || 'localhost'
-const port = process.env.PORT || 3000
-
-if (process.env.NODE_ENV === 'production') {
-  if (hostname === 'localhost') {
-    console.error('Please set HOSTNAME environment variable to your hostname')
-    process.exit(1)
-  }
-}
-
+const hostname = 'localhost'
+const port = 3000
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
@@ -43,3 +35,4 @@ app.prepare().then(() => {
     console.log(`> Ready on http://${hostname}:${port}`)
   })
 })
+
