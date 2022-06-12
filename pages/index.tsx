bcbd8211782a0f9feda86c23cc3a765ed2b137c9
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2022-06-12 11:23:14 +0700

    feat: add button to findword

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,30 @@
-import { FC , Fragment} from 'react'
+import { FC, Fragment } from 'react'
 import NavBar from '@components/NavBar'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-const Home:FC = () => { 
-return (
+const Home: FC = () => {
+  const router = useRouter()
+  const routerFindword = () => {
+    router.push('/findword')
+  }
+  return (
     <Fragment>
       <Head>
         <title>Home</title>
       </Head>
-        <NavBar />
+      <NavBar />
       <p>Hello, World</p>
-      <div><p>Deplayment test</p></div>
+      <div>
+        <p>Deplayment test</p>
+      </div>
+      {/* useRouter to /findword */}
+      <button
+        onClick={routerFindword}
+        className="m-4 rounded-md bg-orange-100 px-2 py-2 transition duration-150 ease-in-out hover:bg-orange-200 hover:scale-110"
+      >
+        Find Word
+      </button>
     </Fragment>
   )
 }
